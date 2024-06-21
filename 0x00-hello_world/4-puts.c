8a9b6ba1877e9c55e351e13f7fb4363cc94c2b47
CJay_alx <chijiokennabueze@gmail.com>
2024-06-21 04:02:36 +0100

    task 5

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: This is the main function prints using puts
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
